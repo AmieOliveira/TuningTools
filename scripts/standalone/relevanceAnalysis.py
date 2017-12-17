04f9a9c31df33527332726c23fada8538492595a
Amanda Camacho Novaes de Oliveira <amandacnoliveira@poli.ufrj.br>
2017-12-17 00:38:03 -0200

    Fixes in relevance analysis

--- scripts/standalone/relevanceAnalysis.py
+++ scripts/standalone/relevanceAnalysis.py
@@ -6,12 +6,12 @@ from RingerCore import emptyArgumentsPrintHelp
 mainParser = ArgumentParser( description = 'Calculate relevance of a specific input variable',
                              add_help = False )
 dataArgs = mainParser.add_argument_group( "Required arguments", "" )
-dataArgs.add_argument( '-iO', '--originalFile', action='store',
+dataArgs.add_argument( '-inO', '--originalFile', action='store',
                        metavar='o_file', required=True,
                        help="""The crossValStatAnalysis file of the discriminator trained
                                with ALL variables without modifications"""
                      )
-dataArgs.add_argument( '-iM', '--modifiedFile', action='store',
+dataArgs.add_argument( '-inM', '--modifiedFile', action='store',
                        metavar='m_file', required=True, nargs='+',
                        help="""The crossValStatAnalysis files of the discriminators 
                                trained without one of its variables (substituted by its 
@@ -25,6 +25,12 @@ dataArgs.add_argument( '-iM', '--modifiedFile', action='store',
 dataArgs.add_argument( '-o', '--outputFileBase', action='store',
                        metavar='out', required=True,
                        help="""Base name for the output file"""
+                     )
+
+dataArgs.add_argument( '--label', action='store',
+                       required=False, nargs='+',
+                       help="""Name of the variables that are 
+                               being analized."""
                      )
 
 
@@ -52,6 +58,13 @@ delta = []
 meanDelta = []
 stdDevDelta = []
 
+nVar = len(args.modifiedFile)
+logger.info('Total of %i variables analyzed.'%(nVar))
+if args.label:
+  if len(args.label) != nVar:
+    logger.warning('Number of labels given do not match the number of variables. Unsetting labels usage.')
+    args.label = None
+
 # reference = {}
 # for sort in args.o_file['infoPPChain'].keys():
 #   reference[sort] = args.o_file['OperationPoint_Offline_LH_Medium_SP']['config_006'][sort]['infoOpBest']['sp']
@@ -64,13 +77,21 @@ for var in range(len(args.modifiedFile)):
   mFile = load(args.modifiedFile[var])
   tmp = []
   for s in range(len(oFile['infoPPChain'].keys())):
-    tmp = tmp + [ oFile['OperationPoint_Offline_LH_Medium_SP']['config_006']['sort_%1.3i'%(s)]['infoOpBest']['sp'] \
-        - mFile['OperationPoint_Offline_LH_Medium_SP']['config_006']['sort_%1.3i'%(s)]['infoOpBest']['sp'] ]
-    logger.debug('Variable %i, Sort %i: delta SP value %f'%(var,s,tmp[s]))
+    tmp = tmp + \
+        [ oFile['OperationPoint_Offline_LH_Medium_SP']['config_006']['sort_%1.3i'%(s)]['infoOpBest']['sp'] - \
+        mFile['OperationPoint_Offline_LH_Medium_SP']['config_006']['sort_%1.3i'%(s)]['infoOpBest']['sp'] ]
+    if args.label:
+      logger.debug('Variable %s, Sort %i: delta SP value %f'%(args.label[var].upper(),s,tmp[s]))
+    else:
+      logger.debug('Variable %i, Sort %i: delta SP value %f'%(var,s,tmp[s]))
   delta = delta + [tmp]
   meanDelta = meanDelta + [ sum(tmp)/len(tmp) ]
   stdDevDelta = stdDevDelta + [ sqrt( sum( [ (tmp[i] - meanDelta[var])**2 for i in range(len(tmp)) ] )/len(tmp) ) ]
-  logger.info('Variable %i: delta SP value (%f +- %f) '%(var,meanDelta[var], stdDevDelta[var]))
+  if args.label:
+    logger.info('Variable %s: delta SP value (%f +- %f) '%\
+        (args.label[var].upper(), meanDelta[var], stdDevDelta[var]))
+  else:
+    logger.info('Variable %i: delta SP value (%f +- %f) '%(var, meanDelta[var], stdDevDelta[var]))
 
 
 # Generate output file
@@ -82,8 +103,6 @@ from ROOT import TCanvas, TGraphErrors, TImage
 from ROOT import gROOT
 from array import array
 
-from RingerCore import keyboard; keyboard()
-
 logger.verbose('Creating graphics...')
 canvas = TCanvas( 'c', 'Relevance of training variables', 200, 10, 700, 500 )
 canvas.SetGrid()
@@ -93,8 +112,8 @@ canvas.GetFrame().SetBorderSize( 12 )
 n = len(meanDelta)
 var = array( 'f', range(n) )
 eVar = array( 'f', [0]*n )
-delt = array( 'f', meanDelta )
-eDelt = array( 'f', stdDevDelta )
+delt = array( 'f', meanDelta*100 )
+eDelt = array( 'f', stdDevDelta*100 )
 
 gr = TGraphErrors( n, var, delt, eVar, eDelt )
 gr.SetTitle( 'Relevance' )
@@ -104,6 +123,9 @@ gr.Draw( 'ALP' )
 
 canvas.Update()
 
+fileName = "%s.png"%(args.outputFileBase)
 img = TImage.Create()
 img.FromPad(canvas)
-img.WriteImage( "%s.png"%(args.outputFileBase) )
+logger.verbose("Saving file...")
+img.WriteImage( fileName )
+logger.info("Saved file '%s'"%(fileName))
